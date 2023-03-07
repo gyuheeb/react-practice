0b3d25ea8b174702408ddae63dc4f8ef55a07d3b
gyuheeb <gyuhee4529@naver.com>
2023-03-07 13:55:59 +0900

    webpack #3

---
+++ 8.webpack-practice/ex02/webpack.config.js
@@ -0,0 +1,13 @@
+const path =require('path');
+
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve('src/index.js'),
+    output:{
+        path: path.resolve('public'),
+        filename:'assets/js/bundle.js'
+
+    }
+
+}
