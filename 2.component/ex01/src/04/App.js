4a7c709995ef4289e469c624d5c2e2c2083ec6b9
gyuheeb <gyuhee4529@naver.com>
2023-03-09 09:40:21 +0900

    component property

--- 2.component/ex01/src/04/App.js
+++ 2.component/ex01/src/04/App.js
@@ -5,7 +5,15 @@ function App() {
   return (
     <div id='App'>
       <MyComponent 
-        props02={10}/>
+        props02={10}
+        props03={true}
+        props04={{no:1, name:"roopy"}}
+        props05={['hello','world','react.js','es6']}
+        props06={() => '함수'}  //return '함수'
+        props07={10}
+        props08={[true, false, true, true]}
+        props09={{no:1, name:'roopy', email: 'roopy@gmail.com'}}
+        />
     </div>
   );
 }
