c727375e4e2e9b4d37d604acf6f62975e7a98bc2
gyuheeb <gyuhee4529@naver.com>
2023-03-13 12:04:37 +0900

    event #2

---
+++ 2.component/ex03/src/02/TitleBar01.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+export default class TitelBar01 extends Component {
+    constructor(props){
+        super(props);
+        //this.no VS this.state.no
+        this.no = 0;
+        this.state= {no: 0}
+
+    }
+    onClickHandler(){
+        this.no++;
+        console.log(`TitleBar01 Clicked: ${this.no}`);
+        this.setState({
+            no: this.state.no +1
+        });
+        console.log(`TitleBar01 Clicked: ${this.state.no}`);
+
+    }
+    render() {
+        return (
+            <div>
+                <h1 onClick={this.onClickHandler.bind(this)}
+                >
+                Function Handler
+                <br />
+                {
+                  //this.no VS this.state.no
+                 `${this.no} VS ${this.state.no}`
+                }
+                </h1>
+                
+            </div>
+        );
+    }
+}
