727347ab72e0e500d9c140a1ffd81236f98732e2
gyuheeb <gyuhee4529@naver.com>
2023-03-15 09:17:04 +0900

    Clock #finish

--- 2.component/ex05/src/04/App.js
+++ 2.component/ex05/src/04/App.js
@@ -3,27 +3,39 @@ import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
+    const getCurrentClockTime = () =>{
+        const now = new Date();
+        const hours = now.getHours();
+
+        return {
+            hours: ('0' + (hours > 12 ? hours - 12 : hours)).slice(-2),
+            minutes: ('0' + now.getMinutes()).slice(-2),
+            seconds: ('0' + now.getSeconds()).slice(-2),
+            session: hours > 12 ? 'pm': 'am'
+        }
+    }
+    const [currenttime, setCurrentTime]= useState(getCurrentClockTime());
     const[tick, setTick] = useState(new Date());
 
     useEffect(() => {
         const Timer = setInterval(() => {
-          const time = new Date();
-          setTick(new Date())
+          setCurrentTime(getCurrentClockTime());
+          setTick(t => t+1);
         }, 1000);
-        return (() => clearInterval(id))
+        return (() => clearInterval(Timer))
       }, []);
     
     return (
         <div>
-           
+           <span>{tick}</span>
             {
-                tick.getSeconds() % 10 === 0 ?
+                tick % 10 === 0 ?
                 null:
             <Clock
                     message={'ex05: useEffect Hook example'}
-                    hours={tick.getHours()}
-                    minutes={tick.getMinutes()}
-                    seconds={tick.getSeconds()}/>
+                    hours={currenttime.hours}
+                    minutes={currenttime.minutes}
+                    seconds={currenttime.seconds}/>
                  
 
             }
--- 2.component/ex05/src/04/Clock.js
+++ 2.component/ex05/src/04/Clock.js
@@ -4,17 +4,14 @@ import SessionAmPm from "./SessionAmPm";
 import './assets/scss/Clock.scss';
 
 export default function Clock({message, hours, minutes, seconds}) {
-
-
-
     return (
         <div className={'clock-display'}>
             <h2>{message}</h2>
             <div className={'Clock'}>
-                <SevenSegmentLED number={('0'+(hours>12 ? hours - 12 : hours)).slice(-2)} colon={true}/>
-                <SevenSegmentLED number={('0'+ minutes).slice(-2)} colon={true}/>
-                <SevenSegmentLED number={('0'+ seconds).slice(-2)} colon={false}/>
-                <SessionAmPm session={parseInt(hours) >= 12 ? 'pm' : 'am'}/>
+                <SevenSegmentLED number={hours} colon={true}/>
+                <SevenSegmentLED number={minutes} colon={true}/>
+                <SevenSegmentLED number={seconds} colon={false}/>
+                <SessionAmPm session={parseInt(hours) > 12 ? 'pm' : 'am'}/>
             </div>
         </div>
     );
