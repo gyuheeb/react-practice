02b9c20eca91367b4c8dd3a72072f2431fc26fb2
gyuheeb <gyuhee4529@naver.com>
2023-03-16 14:02:27 +0900

    routing #finish

---
+++ 3.routing/src/08/component/about/Location.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Location = () => {
+    return (
+        <div className={styles.Me}>
+                <h2>부산입니다.</h2>
+            </div>
+    );
+};
+
+export default Location;
